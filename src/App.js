d2dc87bbaab44b7ac9ef6d0d77e97b9e9ac29c61
FL_LPT-190 <balapavankumar>
2021-11-30 21:42:01 +0530

    react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HoverCounter from './Hoverconunter';
+
+function App() {
+  return (
+    <div className="App">
+      <HoverCounter></HoverCounter>
+    </div>
+  );
+}
+
+export default App;
